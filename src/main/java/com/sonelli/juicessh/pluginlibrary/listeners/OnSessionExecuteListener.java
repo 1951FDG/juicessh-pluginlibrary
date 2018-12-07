5dc7ffc639b8be12112c920866ba53c0a75403e4
1951FDG <1951FDG@users.noreply.github.com>
2018-12-07 17:12:22 +0100

    Fix warning

--- src/main/java/com/sonelli/juicessh/pluginlibrary/listeners/OnSessionExecuteListener.java
+++ src/main/java/com/sonelli/juicessh/pluginlibrary/listeners/OnSessionExecuteListener.java
@@ -5,5 +5,5 @@ import android.support.annotation.NonNull;
 public interface OnSessionExecuteListener {
     void onCompleted(int returnCode);
     void onOutputLine(@NonNull String line);
-    void onError(int reason, @NonNull String Message);
+    void onError(int reason, @NonNull String message);
 }
